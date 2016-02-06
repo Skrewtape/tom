0fff6cfa54597b6dfad36e4665fb9638a89810cb
agoldma <yanetut@gmail.com>
2016-02-06 08:10:17 -0500

    Added imports to User model

--- back/app/types/User.py
+++ back/app/types/User.py
@@ -3,6 +3,10 @@
 """Model object for a user"""
 from app import DB
 from passlib.hash import sha512_crypt
+from sqlalchemy.exc import ArgumentError
+from flask_restless.helpers import to_dict
+
+import re
 
 Role_user_mapping = DB.Table(
     'role_user',
@@ -15,10 +19,21 @@ class User(DB.Model):
 # pylint can't find SQLAlchemy's __init__() method for some reason
     """Model object for a user"""
     user_id = DB.Column(DB.Integer, primary_key=True)
-    username = DB.Column(DB.String(80), unique=True)
-    email = DB.Column(DB.String(120), unique=True)
+    username = DB.Column(DB.String(80), unique=True, nullable=False)
+    email = DB.Column(DB.String(120), unique=True, nullable=False)
     password_crypt = DB.Column(DB.String(134))
-
+    @DB.validates('email')
+    def validate_email(self,key,address):
+        if re.match(r"[^@]+@[^@]+\.[^@]+",address):
+            return address
+        raise ArgumentError
+        
+    @DB.validates('username')
+    def validate_username(self,key,username):
+        if len(username) <= 25:
+            return username
+        raise ArgumentError                
+    
     def crypt_password(self, password):
         """Encrypt a plaintext password and store it"""
         self.password_crypt = sha512_crypt.encrypt(password)
@@ -54,3 +69,11 @@ class User(DB.Model):
     def get_id(self):
         """Get the user's id"""
         return self.user_id
+
+    def to_dict(self):
+        user = to_dict(self)
+        del user['password_crypt']
+        #if Admin_permission.can() or self.user_id == current_user.user_id:
+        #    user['roles'] = [r.name for r in self.roles]
+        return user
+            
