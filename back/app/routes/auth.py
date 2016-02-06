3d2d1ca9a92bcb12f3d780e1a4c6886700bfe3fa
agoldma <yanetut@gmail.com>
2016-02-06 08:27:32 -0500

    commented out uneeded auth.py code

--- back/app/routes/auth.py
+++ back/app/routes/auth.py
@@ -17,14 +17,6 @@ from app.types import User, Role
 
 from app.routes.util import fetch_entity
 
-def user_dict(user):
-    """Turn a User object into a dictionary suitable for serialization"""
-    data = to_dict(user)
-    del data['password_crypt']
-    if Admin_permission.can() or user.user_id == current_user.user_id:
-        data['roles'] = [r.name for r in user.roles]
-    return data
-
 @App.route('/user', methods=['GET'])
 @login_required
 @Admin_permission.require(403)
@@ -39,48 +31,48 @@ def get_roles():
     """Get a list of roles"""
     return jsonify(roles=[to_dict(r) for r in Role.query.all()])
 
-@App.route('/user/current', methods=['PUT'])
-def update_current_user():
-    """Update the current user's data"""
-    # pylint: disable=unexpected-keyword-arg, no-value-for-parameter
-    # pylint doesn't understand the argument name magic that happens with @fetch_entity
-    return update_user(user_id=current_user.user_id)
+# @App.route('/user/current', methods=['PUT'])
+# def update_current_user():
+#     """Update the current user's data"""
+#     # pylint: disable=unexpected-keyword-arg, no-value-for-parameter
+#     # pylint doesn't understand the argument name magic that happens with @fetch_entity
+#     return update_user(user_id=current_user.user_id)
 
-@App.route('/user/<user_id>', methods=['PUT'])
-@login_required
-@fetch_entity(User, 'user')
-def update_user(user):
-    """Update a user's data"""
-    if user.user_id != current_user.user_id:
-        Admin_permission.test(403)
-    input_data = json.loads(request.data)
-    if Admin_permission.can() and 'roles' in input_data:
-        for role in user.roles:
-            if role.name in input_data['roles']:
-                input_data['roles'].remove(role.name)
-            else:
-                user.roles.remove(role)
-        for role_name in input_data['roles']:
-            role = Role.query.filter_by(name=role_name).first()
-            if role is not None:
-                user.roles.append(role)
-    DB.session.commit()
-    return jsonify(user_dict(user))
+# @App.route('/user/<user_id>', methods=['PUT'])
+# @login_required
+# @fetch_entity(User, 'user')
+# def update_user(user):
+#     """Update a user's data"""
+#     if user.user_id != current_user.user_id:
+#         Admin_permission.test(403)
+#     input_data = json.loads(request.data)
+#     if Admin_permission.can() and 'roles' in input_data:
+#         for role in user.roles:
+#             if role.name in input_data['roles']:
+#                 input_data['roles'].remove(role.name)
+#             else:
+#                 user.roles.remove(role)
+#         for role_name in input_data['roles']:
+#             role = Role.query.filter_by(name=role_name).first()
+#             if role is not None:
+#                 user.roles.append(role)
+#     DB.session.commit()
+#     return jsonify(user_dict(user))
 
-@App.route('/user/current', methods=['GET'])
-@login_required
-def get_current_user():
-    """Get information about the current logged in user"""
-    # pylint: disable=unexpected-keyword-arg, no-value-for-parameter
-    # pylint doesn't understand the argument name magic that happens with @fetch_entity
-    return get_user(user_id=current_user.user_id)
+# @App.route('/user/current', methods=['GET'])
+# @login_required
+# def get_current_user():
+#     """Get information about the current logged in user"""
+#     # pylint: disable=unexpected-keyword-arg, no-value-for-parameter
+#     # pylint doesn't understand the argument name magic that happens with @fetch_entity
+#     return get_user(user_id=current_user.user_id)
 
-@App.route('/user/<user_id>', methods=['GET'])
-@login_required
-@fetch_entity(User, 'user')
-def get_user(user):
-    """Get information about a particular user"""
-    return jsonify(user_dict(user))
+# @App.route('/user/<user_id>', methods=['GET'])
+# @login_required
+# @fetch_entity(User, 'user')
+# def get_user(user):
+#     """Get information about a particular user"""
+#     return jsonify(user_dict(user))
 
 @App.route('/login', methods=['PUT'])
 def login():
