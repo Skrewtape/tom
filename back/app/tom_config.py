097633068030ab056087d50e8249c918556ad6b5
Aiton Goldman <agoldma@us.ibm.com>
2016-06-11 21:07:00 -0400

    Can now set a env variable to allow for player login

--- back/app/tom_config.py
+++ back/app/tom_config.py
@@ -1,5 +1,12 @@
+import os
+import sys
+
 scores_per_entry = 5
 max_num_concurrent_entries = 2
 max_unstarted_tokens = 5
 use_stripe = True
-player_login = False
+
+if 'ALLOW_PLAYER_LOGIN' in os.environ:    
+    player_login = True
+else:
+    player_login = False
