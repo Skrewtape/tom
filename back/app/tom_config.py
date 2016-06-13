8e7475fa1857a9c1c09e7f5cb39bb5be5c13066e
Aiton Goldman <agoldma@us.ibm.com>
2016-06-13 14:41:30 -0400

    cleaned up handling of env variable for player login

--- back/app/tom_config.py
+++ back/app/tom_config.py
@@ -1,12 +1,10 @@
 import os
 import sys
 
-scores_per_entry = 5
 max_num_concurrent_entries = 2
 max_unstarted_tokens = 5
-use_stripe = True
-
+use_stripe = False
+player_login = False
 if 'ALLOW_PLAYER_LOGIN' in os.environ:    
     player_login = True
-else:
-    player_login = False
+    
