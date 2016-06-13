9931bc082786abd3c32f7243092da8d3104c8a90
Aiton Goldman <agoldma@us.ibm.com>
2016-06-13 14:40:53 -0400

    init_clean_db.py handles user creation better

--- back/tom_utils_bin/init_clean_db.py
+++ back/tom_utils_bin/init_clean_db.py
@@ -10,20 +10,39 @@ import app
 from app import DB
 import random
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Initialize a TOM db and specify initial users.')
+parser.add_argument('--admin_username', type=str, 
+                    help='admin user name', required=True)
+parser.add_argument('--admin_password', type=str, 
+                    help='admin password', required=True)
+parser.add_argument('--deskworker_username', type=str, 
+                    help='deskworker user name')
+parser.add_argument('--deskworker_password', type=str, 
+                    help='deskworker password')
+parser.add_argument('--scorekeeper_username', type=str, 
+                    help='scorekeeper username')
+parser.add_argument('--scorekeeper_password', type=str, 
+                    help='scorekeeper password')
+
+args = parser.parse_args()
+
+
 def init_users():
 #create users in db
+   create_user(args.admin_username,args.admin_password,ALL_ROLES)
+   if args.scorekeeper_username:
+      create_user(args.scorekeeper_username,args.scorekeeper_password,['scorekeeper'])
+   if args.deskworker_username:
+      create_user(args.deskworker_username,args.deskworker_password,['desk'])
 
-   default_users = [['avi', 'finkel.org', ALL_ROLES],
-                    ['elizabeth', 'papa.org', ALL_ROLES],
-                    ['admin', 'papa.org', ['admin']],
-                    ['scorekeeper', 'papa.org', ['scorekeeper']],
-                    ['desk', 'papa.org', ['desk']],
-   ]    
-   for user in default_users:
-       create_user(user[0],user[0],user[2]) 
-
-
+print "initializing db"
 init_db()            
+print "initializing roles"
 init_roles()
+print "initializing users"
 init_users()
+print "initializing machines"
 init_machines()
+print "all done!"
