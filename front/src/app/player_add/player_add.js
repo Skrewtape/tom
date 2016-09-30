f6dd0f836f2b8cd792fef01f8bfaac7e92a8c103
Aiton Goldman <agoldma@us.ibm.com>
2016-09-30 10:37:40 -0400

    add ifpa lookup to player add

--- front/src/app/player_add/player_add.js
+++ front/src/app/player_add/player_add.js
@@ -1,7 +1,23 @@
 angular.module('app.player_add',['app.player_add.process']);
 angular.module('app.player_add').controller(
     'app.player_add',
-    function($scope, $http, $location, $state, Page, StatusModal, TimeoutResources) {
+    function($scope, $http, $location, $state, Page, StatusModal, TimeoutResources,$resource) {
 	$scope.new_player = {};
-    }
-);
+        $scope.ifpa_results = {};
+        $scope.ifpa_loading=false;
+        $scope.get_ifpa_in_new_tab = function(){
+            var url = "https://www.ifpapinball.com/ajax/searchplayer.php?search="+encodeURIComponent($scope.new_player.first_name+" "+$scope.new_player.last_name);
+            window.open(url,'_blank');
+
+        };
+        $scope.get_ajax_ifpa= function(){
+            $scope.ifpa_loading=true;
+            $scope.ifpa_results = $resource('[APIHOST]/ifpa/:player_name',{player_name:""});
+            $scope.ifpa_results.get({player_name:$scope.new_player.first_name+" "+$scope.new_player.last_name},function(data){
+                $scope.ifpa_loading=false;
+                $scope.ifpa_results = data;
+                
+        });
+        };
+    });
+
